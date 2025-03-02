f5b23a4e55920e60a98432450fc04e9d6ddc5609
EdwinY-UPRM-DEV <edwin.almodovar3@upr.edu>
2025-03-02 17:26:24 -0400

    getters for Station and testing

--- Python Specific/sats.py
+++ Python Specific/sats.py
@@ -1,14 +1,34 @@
+#Station class for scalability
 class Station():
-    def __init__(self, name, downtime_link_active, num_of_sats, maintenance):
-        self.name = name
-        self.downtime_link_active = downtime_link_active
-        self.num_of_sats = num_of_sats
-        self.maintenance = maintenance
+    def __init__(self, name, is_downtime, num_of_sats, maintenance):
+        self.__name = name
+        self.__is_downtime = is_downtime
+        self.__num_of_sats = num_of_sats
+        self.__maintenance = maintenance
+
+    #getters
+    def getName(self): return self.__name
+    def getIsDowntime(self): return self.__is_downtime
+    def getNumOfSats(self): return self.__num_of_sats
+    def getMaintenance(self): return self.__maintenance
+
 
 def main() ->None:
 
+    #initialization of stations
     stationA = Station('Station A', False, 0, False)
-    stationB = Station('Station B', False, 0, False)
+    stationB = Station('Station B', False, 5, False)
+
+    #testing
+    try:
+        print(stationA.num_of_sats)
+        print(stationB.num_of_sats)
+    except:
+        print("Values succesfully obfuscated")
+
+    print(stationA.getNumOfSats())
+    print(stationB.getNumOfSats())
+
 
 
 
